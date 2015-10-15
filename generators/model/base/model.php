dd0263ec6b41dd801527c0c5dbcb7f5e9763bc50
Vitaly Voskobovich <vitali.voskobovich@moonion.com>
2015-10-15 12:39:05 +0300

    Dev

--- generators/model/base/model.php
+++ generators/model/base/model.php
@@ -4,7 +4,7 @@
  */
 
 /* @var $this yii\web\View */
-/* @var $generator voskobovich\gii\generators\model\Generator */
+/* @var $generator yii\gii\generators\model\Generator */
 /* @var $tableName string full table name */
 /* @var $className string class name */
 /* @var $queryClassName string query class name */
@@ -29,76 +29,76 @@ use Yii;
 * This is the model class for table "<?= $generator->generateTableName($tableName) ?>".
 *
 <?php foreach ($tableSchema->columns as $column): ?>
-    * @property <?= "{$column->phpType} \${$column->name}\n" ?>
+ * @property <?= "{$column->phpType} \${$column->name}\n" ?>
 <?php endforeach; ?>
 <?php if (!empty($relations)): ?>
-    *
-    <?php foreach ($relations as $name => $relation): ?>
-        * @property <?= $relation[1] . ($relation[2] ? '[]' : '') . ' $' . lcfirst($name) . "\n" ?>
-    <?php endforeach; ?>
+ *
+<?php foreach ($relations as $name => $relation): ?>
+ * @property <?= $relation[1] . ($relation[2] ? '[]' : '') . ' $' . lcfirst($name) . "\n" ?>
+<?php endforeach; ?>
 <?php endif; ?>
-*/
+ */
 abstract class Base<?= $className ?> extends <?= $extendClassName . "\n" ?>
 {
-/**
-* @inheritdoc
-*/
-public static function tableName()
-{
-return '<?= $generator->generateTableName($tableName) ?>';
-}
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '<?= $generator->generateTableName($tableName) ?>';
+    }
 <?php if ($generator->db !== 'db'): ?>
 
     /**
-    * @return \yii\db\Connection the database connection used by this AR class.
-    */
+     * @return \yii\db\Connection the database connection used by this AR class.
+     */
     public static function getDb()
     {
-    return Yii::$app->get('<?= $generator->db ?>');
+        return Yii::$app->get('<?= $generator->db ?>');
     }
 <?php endif; ?>
 
-/**
-* @inheritdoc
-*/
-public function rules()
-{
-return [<?= "\n            " . implode(",\n            ", $rules) . ",\n        " ?>];
-}
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [<?= "\n            " . implode(",\n            ", $rules) . ",\n        " ?>];
+    }
 
-/**
-* @inheritdoc
-*/
-public function attributeLabels()
-{
-return [
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
 <?php foreach ($labels as $name => $label): ?>
-    <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
+            <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
 <?php endforeach; ?>
-];
-}
+        ];
+    }
 <?php foreach ($relations as $name => $relation): ?>
 
     /**
-    * @return \yii\db\ActiveQuery
-    */
+     * @return \yii\db\ActiveQuery
+     */
     public function get<?= $name ?>()
     {
-    <?= $relation[0] . "\n" ?>
+        <?= $relation[0] . "\n" ?>
     }
 <?php endforeach; ?>
 <?php if ($queryClassName): ?>
-    <?php
+<?php
     $queryClassFullName = ($generator->ns === $generator->queryNs) ? $queryClassName : '\\' . $generator->queryNs . '\\' . $queryClassName;
     echo "\n";
-    ?>
+?>
     /**
-    * @inheritdoc
-    * @return <?= $queryClassFullName ?> the active query used by this AR class.
-    */
+     * @inheritdoc
+     * @return <?= $queryClassFullName ?> the active query used by this AR class.
+     */
     public static function find()
     {
-    return new <?= $queryClassFullName ?>(get_called_class());
+        return new <?= $queryClassFullName ?>(get_called_class());
     }
 <?php endif; ?>
 }
--- generators/model/default/query.php
+++ generators/model/default/query.php
@@ -19,33 +19,33 @@ echo "<?php\n";
 namespace <?= $generator->queryNs ?>;
 
 /**
-* This is the ActiveQuery class for [[<?= $modelFullClassName ?>]].
-*
-* @see <?= $modelFullClassName . "\n" ?>
-*/
+ * This is the ActiveQuery class for [[<?= $modelFullClassName ?>]].
+ *
+ * @see <?= $modelFullClassName . "\n" ?>
+ */
 class <?= $className ?> extends <?= '\\' . ltrim($generator->queryBaseClass, '\\') . "\n" ?>
 {
-/*public function active()
-{
-$this->andWhere('[[status]]=1');
-return $this;
-}*/
+    /*public function active()
+    {
+        $this->andWhere('[[status]]=1');
+        return $this;
+    }*/
 
-/**
-* @inheritdoc
-* @return <?= $modelFullClassName ?>[]|array
-*/
-public function all($db = null)
-{
-return parent::all($db);
-}
+    /**
+     * @inheritdoc
+     * @return <?= $modelFullClassName ?>[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
 
-/**
-* @inheritdoc
-* @return <?= $modelFullClassName ?>|array|null
-*/
-public function one($db = null)
-{
-return parent::one($db);
-}
+    /**
+     * @inheritdoc
+     * @return <?= $modelFullClassName ?>|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
 }
